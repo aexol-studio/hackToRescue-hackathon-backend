3fbfa39141f27a2449634e613aef471b03f76ef5
TakSeBiegam <kurylo.ao@gmail.com>
2023-12-07 00:54:20 +0100

    chore: late commit

--- src/Query/getCityParameters.ts
+++ src/Query/getCityParameters.ts
@@ -5,11 +5,11 @@ import { orm } from '../utils/orm.js';
 export const handler = async (input: FieldResolveInput) =>
   resolverFor('Query', 'getCityParameters', async ({ city, startDate, endDate, stationId }) => {
     const o = await orm();
-    const t = await o('file_stations').collection.findOne({
+    const t = await o('file_stations').collection.find({
       city,
       ...(stationId && { stationId }),
       ...(startDate && { 'parameters.startDate': { $gte: startDate } }),
       ...(endDate && { 'parameters.endDate': { $lte: endDate } }),
-    });
+    }).toArray();
     return t;
   })(input.arguments);
